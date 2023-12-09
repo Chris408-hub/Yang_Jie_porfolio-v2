a8a915e81234e47d32862835292bdbb831ec5d94
Chris408-hub <112998909+Chris408-hub@users.noreply.github.com>
2023-12-09 00:21:09 -0500

    adding sass

---
+++ js/lightbox.js
@@ -0,0 +1,20 @@
+
+
+
+// //thanks page close button
+//   const thanksBtn = document.querySelector('#close-btn');
+//   const openBtn = document.querySelector('#message-btn');
+//   const lightbox = document.querySelector('#thanks-light-box');
+
+
+//   export function closePage() {
+//     lightbox.style.display = 'none';
+//     };
+
+//     // export
+//    export function openPage() {
+//       lightbox.style.display = 'block';
+//     };
+//   openBtn.addEventListener('click', openPage);
+//   thanksBtn.addEventListener('click', closePage);
+  
